691b38e2d45bdb46cd5bd50a1dc7c68b82be8d8c
Rex Ye <rxye1991@gmail.com>
2020-01-03 22:59:20 +0000

    Automatically backed up by Learn

--- demo.js
+++ demo.js
@@ -39,11 +39,11 @@ function likeCallback(e) {
 // code when an "event" is fired. That's Pillar 2, event handling. Uncomment
 // this code.
 
-//for (let glyph of articleHearts) {
-  //glyph.addEventListener("click", likeCallback);
-//}
+for (let glyph of articleHearts) {
+  glyph.addEventListener("click", likeCallback);
+}
 
-// STEP 4: 
+// STEP 4:
 
 // When all the STEPs' code changes have been complete, you'll be able to see a
 // working demonstration of our reference example. Sure, it's maybe not as
